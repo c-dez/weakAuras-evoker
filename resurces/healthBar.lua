766a993d6b4c41a05ff71237d29d7a12c0abb0fa
c-dez <hdezcruzcarlos@gmail.com>
2023-12-05 01:35:49 -0600

    added to healthBar.lua mana text

--- resurces/healthBar.lua
+++ resurces/healthBar.lua
@@ -1,7 +1,15 @@
 function()
+    -- formula para calcular porcentaje actual de vida player
     local currentHealth = UnitHealth('player')
     local maxHealth = UnitHealthMax('player') 
-    local healthPercentage = currentHealth/maxHealth * 100
-    return string.format('%.0f', healthPercentage) 
+    local healthPercentage = currentHealth / maxHealth * 100
+    --calcula porcentaje mana
+    local currentMana = UnitPower("player",0)
+    local maxMana = UnitPowerMax("player",0)
+    local manaPercentage = currentMana / maxMana * 100
+
+    return string.format('%.0f', healthPercentage) , string.format('%.0f', manaPercentage)
 end
--- formula para calcular porcentaje actual de vida player
+
+
+
